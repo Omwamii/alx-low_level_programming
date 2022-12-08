8c4fa29a2291d37cfe44a651f80fd7f92a18ee21
Ian <ianalpha@protonmail.com>
2022-12-08 09:43:49 -0800

    newline

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,9 @@
   *main - This fn prints a string
   *
   *Return: returns status
-  *Return: 0
   */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
