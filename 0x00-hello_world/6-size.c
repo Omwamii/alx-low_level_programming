17e2f80c4214f254c37f97c2ae99df097ad489d3
Ian <ianalpha@protonmail.com>
2022-12-08 11:10:43 -0800

    str

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(long));
-	printf("Size of a long long int: %zu bytes(s)\n", (unsigned long)sizeof(long long));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long));
 	printf("Size of a float: %zu bytes(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
