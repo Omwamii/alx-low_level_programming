5fbbe3d8c9364bc2192616d39636540e83a4bf1d
Ian <ianalpha@protonmail.com>
2022-12-08 10:05:27 -0800

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,14 @@
 /**
   *main - prints size of types
   *
-  *Return - exit status
-  *Return : 0
+  *Return - exit status of fn
   */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long));
+	printf("Size of a long long int: %d bytes(s)", sizeof(long long));
 	printf("Size of a float: %d bytes(s)", sizeof(float));
 	return (0);
 }
