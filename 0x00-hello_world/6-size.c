1b6c591e48825798c5f818dab7680e0b69dad2f2
Ian <ianalpha@protonmail.com>
2022-12-08 11:27:09 -0800

    pusher

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
   */
 int main(void)
 {
-	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %zu bytes(s)\n", (unsigned long)sizeof(long long));
