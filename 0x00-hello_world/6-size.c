0911be08d61d0ca082d062712385d7e4c65a4443
Ian <ianalpha@protonmail.com>
2022-12-08 10:12:40 -0800

    longg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %zu bytes(s)", sizeof(long long));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long));
 	printf("Size of a float: %d bytes(s)", sizeof(float));
 	return (0);
 }
