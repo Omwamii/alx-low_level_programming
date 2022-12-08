4e71b7466dd2c0c7015a95dc364ff517dbecd187
Ian <ianalpha@protonmail.com>
2022-12-08 09:40:55 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+  *main - This fn prints a string
+  *
+  *Return: returns status
+  *Return: 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
