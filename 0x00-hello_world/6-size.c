c4ae0331780b4f3409ce96405c82f161247ddbdf
Ian <ianalpha@protonmail.com>
2022-12-08 10:35:20 -0800

    b

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(c));
 	printf("Size of a float: %lu bytes(s)", sizeof(f));
 	return (0);
 }
