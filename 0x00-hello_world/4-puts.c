d4217223f6d6572035627fb4b6f70e0cb117be47
Ian <ianalpha@protonmail.com>
2022-12-08 09:14:44 -0800

    added documentatio and parenthesis

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
+/**main - This function prints a statement using puts
+  *@arg : "Programming is like building a multilingual puzzle
+  *
+  *return : 0
+  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
