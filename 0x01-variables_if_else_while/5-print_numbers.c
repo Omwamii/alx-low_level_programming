1909f48f0ee35c8c588e3146660781acf03b6248
Ian <ianalpha@protonmail.com>
2022-12-09 06:46:38 -0800

    printnums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+  *main - entry point
+  *
+  *Return: Always 0
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
