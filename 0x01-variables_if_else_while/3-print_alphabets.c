7ba82b97241dc75dad8e2f131df8487f2008945a
Ian <ianalpha@protonmail.com>
2022-12-09 06:24:20 -0800

    upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+/**
+  *main -entry point
+  *
+  *Return: Always 0
+  */
+int main(void)
+{
+	int x;
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
