4da2ac96a2e7acaf860e0363b667a05776ec16c9
Ian <ianalpha@protonmail.com>
2022-12-09 14:21:36 -0800

    possible twos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+  *main - entry point
+  *Return: ret 0 if success
+  */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			if (x < y)
+			{
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
