aca0ea862c6f56fdc256268bb49a0a795de44e4c
Ian <ianalpha@protonmail.com>
2022-12-09 06:36:35 -0800

    all except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+/**
+  *main - entry point
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{	if (x == 'e' || x == 'q')
+			continue;
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
