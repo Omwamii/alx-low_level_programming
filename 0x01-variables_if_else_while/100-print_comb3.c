539827e9253c01761e2d810c3ce62d12f180f8a4
Ian <ianalpha@protonmail.com>
2022-12-09 13:42:41 -0800

    final 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,16 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = '0'; x <= '9'; x++)
+	for (x = '0'; x<= '9'; x++)
 	{
 		for (y = '1'; y <= '9'; y++)
 		{
-			if (!(x == y || x > y)) 
+			if(!(x == y || x > y))
 			{
 				putchar(x);
 				putchar(y);
 
-				if (!(x == '8' && y == '9'))
+				if(!(x == '8' && y == '9'))
 				{
 					putchar(',');
 					putchar(' ');
