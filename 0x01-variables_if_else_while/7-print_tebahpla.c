cfff036921100a87c1f9d74b739dd939bc600104
Ian <ianalpha@protonmail.com>
2022-12-09 07:01:34 -0800

    alpha reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+  *main - entry point
+  *
+  *Return: Always 0
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
