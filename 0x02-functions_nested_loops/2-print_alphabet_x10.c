989759c8ed724323fa87f9bdcd375fda016f3bc5
Ian <ianalpha@protonmail.com>
2022-12-14 10:39:31 -0800

    alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+  *print_alphabet_x10 - prints alphabet 10 times
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	int g;
+
+	for (g = 0; g < 10; g++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+			_putchar('\n');
+		}
+	}
+}
