fb627c6ce83c993ca624ffae7506147d4da988b3
Ian <ianalpha@protonmail.com>
2022-12-14 11:34:33 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+  *print_sign - prints sign of num
+  *@n: parameter
+  *Return: 1 if pos, 0 if 0 ,-1 if neg
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
