377668f8cc3b609fe4d0d00b57fca74d35aae18b
Ian <ianalpha@protonmail.com>
2022-12-14 11:54:32 -0800

    abs value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,19 +8,10 @@
   */
 int _abs(int i)
 {
-	if (i >  0)
-	{
+	if (i > 0)
 		return (i);
-	}
-
 	else if (i < 0)
-	{
-		i = -(i);
-		return (i);
-	}
-
-	else 
-	{
+		return (-(i));
+	else
 		return (0);
-	}
 }
