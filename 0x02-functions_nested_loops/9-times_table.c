aa2065251f05a8ec2015282b81a269374b2551a3
Ian <ianalpha@protonmail.com>
2022-12-14 14:18:12 -0800

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+  *times_table - print format
+  */
+void times_table(void)
+{
+	int a, b, c, d, e;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if (c > 9)
+			{
+				d = c % 10;
+				e = (c - d) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
