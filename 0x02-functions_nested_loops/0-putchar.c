3eb9e321eda54070119f05f3a89ce4ab99cd6141
Ian <ianalpha@protonmail.com>
2022-12-14 10:00:20 -0800

    brt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,11 +9,16 @@
 int main(void)
 {
 	char x[] = "_putchar\n";
+
 	_putchar(x);
 	return (0);
 }
-/*
+/**
+  *_putchar - writes character c to output
+  *@c: char to print
+  *Return: 0 on success
+  */
 int _putchar(char *c)
 {
 	return (write(1,&c, 1));
-}*/
+}
