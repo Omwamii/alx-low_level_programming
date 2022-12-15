e1d5d5642b3555a4717e0242eaa91715a9289f0d
Ian <ianalpha@protonmail.com>
2022-12-15 14:42:06 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,14 @@ void more_numbers(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
