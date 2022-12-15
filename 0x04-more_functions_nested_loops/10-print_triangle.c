43c6a4757b704545b621a9fd366c41ca481604ee
Ian <ianalpha@protonmail.com>
2022-12-15 15:45:11 -0800

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,27 @@
   */
 void print_triangle(int size)
 {
-	int j, i;
-
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = i; j < size; j++)
-		{
-			_putchar(' ');
-		}
-		for (j = 1; j <= i; j++)
-		{
-			_putchar(35);
-		}
 		_putchar('\n');
 	}
-	_putchar('\n');
+
+	else
+	{
+		int j, i;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
 }
