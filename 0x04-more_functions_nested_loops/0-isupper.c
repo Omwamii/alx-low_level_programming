44ca88bc3e14385f5e690fa7d37e562da05d6492
Ian <ianalpha@protonmail.com>
2022-12-15 12:44:47 -0800

    sls

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-  *_isupper - checks if uppercase
-  *@c: int char  to be checked
-  *
-  *Return: 1 if uppercase, 0 if otherwise
-  */
+ *_isupper - checks if uppercase
+ *@c: int char  to be checked
+ *
+ *Return: 1 if uppercase, 0 if otherwise
+ */
 
 int _isupper(int c)
 {
