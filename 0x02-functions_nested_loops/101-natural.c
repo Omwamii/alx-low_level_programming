107609de76027f2850536a4f9d2d94fe90fbfe95
Ian <ianalpha@protonmail.com>
2022-12-15 16:23:47 -0800

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>>
+
+/**
+  *main - print natural nos below 1024 thats
+  *are multiples of 3 , 5
+  *
+  *Return: 0
+  */
+int main(void)
+{
+	int a, b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
