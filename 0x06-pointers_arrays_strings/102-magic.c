855268398df8c000695f43a86312901de24baa4d
Ian <ianalpha@protonmail.com>
2022-12-21 16:21:56 -0800

    magicio

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,7 +8,14 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
+	/*
+	 *write your linr of code here.
+	 *remember..
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 *you are not allowed to code anything else than this line
+	 */
 	*(p + 5) = 9;
 
 	printf("a[2] = %d\n", a[2]);
