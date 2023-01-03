52531a1e1ad356a6eead1b39429acf695b990103
Omwamii <ianalpha@proton.mail>
2023-01-04 00:36:35 +0300

    strstr c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,40 +1,43 @@
 #include "main.h"
-
+#define NULL 0
 /**
- * _strstr - finds occurence of substring in another str
- * @haystack: major string
- * @needle: str with substring
+ *_strstr - finds occurence of substring in another str
+ *@haystack: major string
+ *@needle: str with substring
  *
- * Return: pointer to first substr character
+ *Return: pointer to first substr character
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int count = 0;
+	int i = 0, j, x;
+	
+	if (needle[0] == '\0')
+		return (haystack);
 
-	while (*haystack != '\0')
+	while (haystack[i] != '\0')
 	{
-		if (*haystack == *needle)
+		if (haystack[i] == needle[0])
 		{
-			count++;
-			needle++;
-			haystack++;
+			x = 1, j = 0;
+
+			while (needle[j] != '\0')
+			{
+				if (haystack[x] == needle[j])
+					x++, j++;
+
+				else
+					break;
+			}
+			
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
 		}
 
-		else
-		{
-			if (*needle != '\0')
-			{
-				for (; count > 0;)
-				{
-					needle--;
-					count--;
-				}
-				haystack++;
-			}
-			else
-				return (haystack - count);
-		}
+		i++;
 	}
+
 	return (NULL);
 }
