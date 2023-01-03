0b26451fced7b72fa93b12eb67609aff59e08051
Omwamii <ianalpha@proton.mail>
2023-01-03 19:12:39 +0300

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		*(dest + i) = *(src + i);
+		for (i = 0; i < size; i++)
+		{
+			*(dest + i) = *(src + i);
+		}
 	}
-
 	return (dest);
 }
