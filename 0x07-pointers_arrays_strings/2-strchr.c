a32b449c0bead10eae784036a1886bc40f68ba82
Omwamii <ianalpha@proton.mail>
2023-01-03 19:29:03 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strchr - finds occurence of char in a string
- * @s: pointer to string
- * @c: character to be scanned
+ *_strchr - finds occurence of char in a string
+ *@s: pointer to string
+ *@c: character to be scanned
  *
- * Return: pointer to first occurence of char
+ *Return: pointer to first occurence of char
  */
 
 char *_strchr(char *s, char c)
