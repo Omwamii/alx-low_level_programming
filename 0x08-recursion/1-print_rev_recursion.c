4e99a3b4872decd4a30b9c1b719db317a9ab4a8a
Omwamii <ianalpha@proton.mail>
2023-01-05 11:47:46 +0300

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,13 +7,11 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
 
-	_print_rev_recursion(&s[i + 1]);
-
-	if (s[i] == '\0')
+	if (*s != '\0')
 	{
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 
 }
