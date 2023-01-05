c26468e042980f571d398b3bf2908dbe392442d1
Omwamii <ianalpha@proton.mail>
2023-01-05 12:33:23 +0300

    strlen c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,8 @@
 
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
-	if (s[count] == '\0')
-		return (count);
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
 	else
-		return (0 + _strlen_recursion(&s[count + 1]));
-
+		return (0);
 }
