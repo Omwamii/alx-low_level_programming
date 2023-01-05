435d0d4a5a9e59be80cec133e282d2405cdd45a0
Omwamii <ianalpha@proton.mail>
2023-01-05 12:04:40 +0300

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - prints length of string
+ *@s: pointer to string
+ *Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (s[count] != '\0')
+	{
+		_strlen_recursion(&s[count + 1]);
+	}
+
+	else
+		return (count);
+
+}
