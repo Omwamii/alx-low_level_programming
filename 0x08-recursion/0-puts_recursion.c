8c567a85616c0d933e77c2f142d9282a14db6878
Omwamii <ianalpha@proton.mail>
2023-01-05 08:32:19 +0300

    puts recursive

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,15 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	int i = 0;
+
+	if (s[i] == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar(*s);
-		_puts_recursion(s++);
+		_putchar(s[i]);
+		_puts_recursion(s[i + 1]);
 	}
 }
