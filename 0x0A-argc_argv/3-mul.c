253f80fac7b245eea30e09b5fb581d0161498a5d
Omwamii <ianalpha@proton.mail>
2023-01-09 15:41:55 +0300

    multiply args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+  *main - entry point
+  *@argc: arg count
+  *@argv: pointer to arg strings
+  *
+  *Return: success result
+  */
+
+int main(int argc, char **argv)
+{
+	if (argc > 2)
+	{
+		int mult = 1;
+		int i;
+
+		for (i= 1; i < argc; i++)
+		{
+			mult *= atoi(*(++argv));
+		}
+
+		printf("%d\n", mult);
+
+		return (0);
+	}
+
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+}
