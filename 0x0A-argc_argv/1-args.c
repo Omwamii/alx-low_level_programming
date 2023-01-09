0d8c757a31c7b51097a1fe631eab162307019910
Omwamii <ianalpha@proton.mail>
2023-01-09 14:38:50 +0300

    print arg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *@argc: argument count
+  *@argv: argument string pointer
+  *
+  *Return: success value
+  */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", (argc - 1));
+
+	return(0);
+}
