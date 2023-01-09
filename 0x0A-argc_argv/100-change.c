aba6728948f2f9a463cd1167c469cbd85ac9dc53
Omwamii <ianalpha@proton.mail>
2023-01-09 18:42:49 +0300

    find least change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,7 +24,10 @@ int main(int argc, char **argv)
 		int change = 0;
 
 		if (num < 0)
+		{
 			printf("0\n");
+			return (0);
+		}
 		else
 		{
 			while (num)
@@ -32,16 +35,27 @@ int main(int argc, char **argv)
 				change++;
 
 				if ((num - 25) >= 0)
+				{
 					num -= 25;
-
+					continue;
+				}
 				if ((num - 10) >= 0)
+				{
 					num -= 10;
+					continue;
+				}
 
 				if ((num - 5) >= 0)
+				{
 					num -= 5;
+					continue;
+				}
 
 				if ((num - 2) >= 0)
+				{
 					num -= 2;
+					continue;
+				}
 
 				num--;
 			}
