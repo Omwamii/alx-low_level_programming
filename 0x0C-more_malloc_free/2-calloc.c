34ce6649805076f713cac8f003dcec88f6542932
Omwamii <ianalpha@protonmail.com>
2023-01-14 00:16:33 +0300

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  *_calloc - allocate mem for array
+  *@nmemb: elements of array
+  *@size: size of variable
+  *
+  *Return: pointer to allocated mem
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (nmemb)
+	{
+		char *dest = malloc(size * nmemb);
+
+		if (dest)
+		{
+			unsigned int i;
+
+			for (i = 0; i < nmemb; i++)
+			{
+				dest[i] = 0;
+			}
+
+			return (dest);
+		}
+
+		return (NULL);
+	}
+
+	return (NULL);
+}
