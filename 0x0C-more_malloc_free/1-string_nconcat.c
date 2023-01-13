f0d9ba1633c293804a5003e4682ee45849223707
Omwamii <ianalpha@protonmail.com>
2023-01-13 22:52:59 +0300

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,12 +10,11 @@
 
 int _strlen(char *str)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	if (str == NULL)
-	{
-		str[0] = '\0';
-	}
+		str = "";
+
 	while (*str != '\0')
 	{
 		i++;
@@ -40,11 +39,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k;
 	char *dest = malloc((size1 + n) + 1);
 
-	if (n >= size2)
-		n = size2;
-
 	if (dest)
 	{
+		if (n >= size2)
+			n = size2;
+
 		for (i = 0; i < size1; i++)
 		{
 			dest[i] = s1[i];
