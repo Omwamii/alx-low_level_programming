1878dd544821929c38ed6f7b7b8f371dfeea7614
Omwamii <ianalpha@protonmail.com>
2023-02-08 23:41:39 +0300

    copy from file_from to file_to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	to = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, mode);
+	to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, mode);
 
 	if (to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
