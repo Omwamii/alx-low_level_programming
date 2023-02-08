2a3149981c4668aab39f4f509747b42d16cd7fbb
Omwamii <ianalpha@protonmail.com>
2023-02-08 22:47:17 +0300

    creates a file and adds text into it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,15 +24,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
-	{
-		writef = write(fd, text_content, len);
-		if (writef < 0)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	if (text_content == NULL)
+		text_content = "";
+
+	writef = write(fd, text_content, len);
+
+	if (writef == -1)
+		return (-1);
 
 	close(fd);
 	return (1);
