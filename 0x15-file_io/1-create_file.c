3539fe3211ff1297289a0f02227154677cf4bb0e
Omwamii <ianalpha@protonmail.com>
2023-02-08 23:00:29 +0300

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,17 +20,21 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	if (len != 0)
+	{
+		writef = write(fd, text_content, len);
 
-	writef = write(fd, text_content, len);
-
-	if (writef == -1)
-		return (-1);
+		if (writef == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
 	close(fd);
 	return (1);
