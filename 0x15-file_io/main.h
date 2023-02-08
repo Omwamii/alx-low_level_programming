f82114e2e66f44de94ccd889f50283070ec93b3a
Omwamii <ianalpha@protonmail.com>
2023-02-09 00:48:06 +0300

    elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <elf.h>
+#include <sys/mman.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
