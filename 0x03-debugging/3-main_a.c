f9512feffa716174acb249378a744e2dcc682bbf
Omwamii <ianalpha@protonmail.com>
2023-02-17 15:48:08 +0300

    supporter mains

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -3,7 +3,7 @@
 /**
   * main - takes a date and prints how many days are left in the year, taking
   *        leap years into account
-  * Return: 0
+  * Return: 0 (success val)
   */
 int main (void)
 {
--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,8 +2,7 @@
 
 
 /**
-  * main - takes a date and prints how many days are left in the year, taking
-  * leap years into account
+  * main - entry point
   * Return: 0
   */
 int main(void)
