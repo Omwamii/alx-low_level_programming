2c0578619e750a3b87124d3c30815bb1eca802c3
omwami <ianalpha@protonmail.com>
2023-03-17 01:28:11 +0300

    add node at specific index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)  /* insert at beginning */
 		return(add_dnodeint(h, n));
+
 	for (; idx != 1; idx--)
 	{
 		tmp = tmp->next;
