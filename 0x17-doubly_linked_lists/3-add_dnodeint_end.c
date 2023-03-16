4472b6a9737ce845e154f9ff75cfc2cd1c26a5ab
omwami <ianalpha@protonmail.com>
2023-03-16 23:58:18 +0300

    add node to end of list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	else
 	{
-		while(tmp->next)
+		while (tmp->next)
 			tmp = tmp->next;
 		new->prev = tmp;
 		new->next = NULL; /* end of list*/
