a5886ead73b1340b76e661b4f2a5a5394565da2a
omwami <ianalpha@protonmail.com>
2023-04-08 15:40:50 +0300

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,9 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
+
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
+
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t *));
 
