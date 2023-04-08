6890bb378e1983f276d73f0af7dd7f610155635c
omwami <ianalpha@protonmail.com>
2023-04-08 15:50:38 +0300

    add item to hash_table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = NULL, *tmp = NULL;
 	unsigned long int index;
 
-	if (key == NULL)
+	if (key == NULL || ht == NULL)
 		return (0);
 
 	node = malloc(sizeof(hash_node_t));
