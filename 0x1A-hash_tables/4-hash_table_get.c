37351229015271feff97f7fa4e6e4e69f41967c0
omwami <ianalpha@protonmail.com>
2023-04-08 14:45:35 +0300

    get value of key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets value of key
+ * @ht: hash table ptr
+ * @key: key passed
+ *
+ * Return: value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *tmp = NULL;
+
+	if (index > ht->size) /* out of bounds */
+		return (NULL);
+
+	tmp = ht->array[index];
+	if (tmp == NULL) /* no value set */
+		return (NULL);
+
+	while (tmp)
+	{
+		if (strcmp(key, tmp->key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+
+	return (NULL);
+}
