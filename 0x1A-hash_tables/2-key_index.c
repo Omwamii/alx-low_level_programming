31d6d71e4e91961753ddadf07e21df0192c574ef
omwami <ianalpha@protonmail.com>
2023-04-08 14:25:16 +0300

    provide index given key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,7 +11,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index = hash_djb2(key);
 
-	if (index > size)
-		return (index % size);
-	return (index);
+	return (index % size);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds element to hash table
+ * @ht: ptr to hashtable
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if sucess 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node = NULL, *tmp = NULL;
+	unsigned long int index;
+
+	if (key == NULL)
+		return (0);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	index = key_index(key, (*ht)->size);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+
+	tmp = (*ht)array[index];
+	if (tmp == NULL) /* no item */
+	{
+		(*ht)array[index] = node;
+		return (1);
+	}
+	node->next = tmp;
+	(*ht)array[index] = node;
+	return (1);
+}
