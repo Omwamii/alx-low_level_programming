e7970dff83c20b39418046bec5f6dcd353ac32bc
omwami <ianalpha@protonmail.com>
2023-04-10 10:27:50 +0300

    set item in hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,8 +10,9 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node = NULL, *tmp = NULL;
+	hash_node_t *node = NULL, *tmp = NULL, *it = NULL;
 	unsigned long int index;
+	char *val;
 
 	if (key == NULL || ht == NULL || *key == '\0' || value == NULL)
 		return (0);
@@ -21,17 +22,30 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 
+	val = strdup(value);
 	node->key = strdup(key);
-	node->value = strdup(value);
 	node->next = NULL;
 
 	tmp = ht->array[index];
+	it = tmp; /* to check if key present with diff value */
 	if (tmp == NULL) /* no item */
 	{
 		ht->array[index] = node;
+		node->value = val;
 		return (1);
 	}
+	while (it) /* update existing key */
+	{
+		if (strcmp(it->key, key) == 0)
+		{
+			free(it->value);
+			it->value = val;
+			return (1);
+		}
+		it = it->next;
+	}
 	node->next = tmp;
+	node->value = val;
 	ht->array[index] = node;
 	return (1);
 }
