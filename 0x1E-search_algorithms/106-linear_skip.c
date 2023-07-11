c135f03ae15825b8f87ea988c7d432ec672ebf95
omwami <ianalpha@protonmail.com>
2023-07-11 23:56:27 +0300

    Jump search with skip linked list

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,43 +1,41 @@
 #include "search_algos.h"
 
 /**
- * jump_list - ptr to head of list
+ * linear_skip - perform jump search on skip list
  * @list: ptr to head of list
- * @size: number of nodes in list
  * @value: value to be searched
  *
  * Return: pointer to where first value located or NULL
  */
-listint_t *jump_list(listint_t *list, size_t size, int value)
+skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	listint_t *tmp = list, *high, *low = list, *found = NULL;
-	size_t count, end, jump;
+	skiplist_t *tmp, *high, *low = list, *found = NULL;
 
 	if (!list)
 		return (NULL);
-	jump = sqrt(size);
-	end = jump;
-	while (tmp->n < value)
+	tmp = list->express; /* first jump */
+	while (1)
 	{
-		while (end >= size)
-			end--, jump--;
-		low = tmp, count = jump;
-		while (count > 0)
-		{
-			tmp = tmp->next;
-			count--;
-		}
-		high = tmp;
-		printf("Value checked at index [%lu] = [%d]\n", tmp->index, tmp->n);
-		end += jump;
 		if (!tmp->next)
 			break; /* reached end of list */
-
+		printf("Value checked at index [%lu] = [%d]\n", tmp->index, tmp->n);
+		if (tmp->n < value)
+		{
+			low = tmp;
+			if (tmp->express)
+				tmp = tmp->express;
+			else /* end of express, go to list end */
+			{
+				while (tmp->next)
+					tmp = tmp->next;
+			}
+		}
+		else
+			break;
 	}
+	high = tmp;
 	printf("Value found between indexes [%lu] and [%lu]\n", low->index,
 			high->index);
-	if (end > size)
-		end = size - 1;
 	tmp = low;
 	while (tmp)
 	{
